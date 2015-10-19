8aa69f0bd818f70648cb3a1e3e83fa4b44dd5c64
Andrew Rothstein <arothste@blackrock.com>
2015-10-19 00:44:33 -0400

    initial commit following the pattern of docker-quantlib-builder

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export APP_NAME=actor-framework
+#docker build -t andrewrothstein/docker-$APP_NAME-builder:fedora_21 fedora_21
+docker build -t andrewrothstein/docker-$APP_NAME-builder:ubuntu_trusty ubuntu_trusty
